212deb9e05a385ad2bd6667ddeb2a2b6d2590563
adalyjen <aidan.jensen@tektronix.com>
2020-03-16 08:39:46 -0700

    Testing out docker-compose env files. Using alternate port for redis. Sharing between services

--- redis_test.py
+++ redis_test.py
@@ -2,8 +2,10 @@ import os
 import redis
 
 redis_host = os.environ["REDIS_HOST"]
+redis_port = os.environ["REDIS_PORT"]
 
-r = redis.Redis(redis_host)
+r = redis.Redis(host=redis_host,
+    port=int(redis_port))
 
 r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
 print(r.get("Bahamas"))
